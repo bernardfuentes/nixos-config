43bf071e6018103e9f849306395b3533f478e5ad
Bernard Fuentes <bernard.fuentes@gmail.com>
2024-10-31 18:20:44 +1030

    Wl-copy

--- modules/home/hyprland/config.nix
+++ modules/home/hyprland/config.nix
@@ -228,7 +228,8 @@
         "$mainMod, mouse_up, workspace, e+1"
 
         # clipboard manager
-        "$mainMod, V, exec, export XDG_CACHE_HOME=/home/$USER/.local/cache; cliphist list | rofi -dmenu -display-columns 2 -window-title '📋' | cliphist decode | wl-copy"
+        "$mainMod, C, exec, export XDG_CACHE_HOME=/home/$USER/.local/cache; cliphist list | rofi -dmenu -display-columns 2 -window-title '📋' | cliphist decode | wl-copy"
+        "$mainMod, V, exec, cliphist list | rofi -dmenu -theme-str 'window {width: 50%;}' | cliphist decode | wl-copy"
       ];
 
       # # binds active in lockscreen
